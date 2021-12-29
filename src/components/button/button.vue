4ef22b2d76ed304929c3b6e3286452a2bf66d2cb
heweijie <heweijie.2013@gmail.com>
2021-12-29 20:40:56 +0800

    feat(init): add test

---
+++ src/components/button/button.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <button
+    class="el-button" 
+    :class="[size ? `el-button--${size}` : '',]"
+  >
+    <slot />
+  </button>
+</template>
+<script setup lang="ts">
+
+import {computed, withDefaults} from 'vue'
+interface Props {
+  size?:""|'small'|'medium'|'large'
+}
+const props = withDefaults(defineProps<Props>(),{
+  size:""
+})
+</script>
